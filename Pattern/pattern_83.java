1f8288453ad6bd0dcd7438e128f9c1edd3b4fb2e
nikita-salunke4578 <nikitasalunke960@gmail.com>
2025-01-08 13:49:13 +0530

    Added Pattern Programs

---
+++ Pattern/pattern_83.java
@@ -0,0 +1,23 @@
+package Pattern;
+
+public class pattern_83 {
+    public static void main(String[] args) {
+        int iLength = 5, jLength = 10;
+        print_pattern(iLength, jLength);
+    }
+
+    private static void print_pattern(int iLength, int jLength) {
+        for (int i = 0; i < iLength; i++) {
+            for (int j = 0; j < jLength; j++) {
+                if (j - i <= 0) {
+                    System.out.print(j + 1);
+                } else if (j + i >= 9) {
+                    System.out.print((char)(40 + (i + j)));
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
