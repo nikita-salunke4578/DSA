eef68b25fe34aab007c0faf8ffd74fa34633b5b6
nikita-salunke4578 <nikitasalunke960@gmail.com>
2025-07-01 23:31:38 +0530

    Added Program of Bubble sort (optimization Code)

---
+++ Array/BubbleSortOpt.java
@@ -0,0 +1,33 @@
+//But if our given array is already sorted then in that case no need to go through all iteration , so here is optimized solution.
+package Array;
+
+import java.util.Arrays;
+
+public class BubbleSortOpt {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int[] result = BubbleSortAlgo(arr);
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
+
+    private static int[] BubbleSortAlgo(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean swap = false;
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swap = true;
+                }
+                if (!swap) {
+                    break;
+                }
+            }
+
+        }
+        return arr;
+    }
+
+}
