093046fd8af43178dac45e0673f540876b34da06
nikita-salunke4578 <nikitasalunke960@gmail.com>
2025-07-14 05:47:35 +0530

    Added Program of BitWise Operators

---
+++ Bit_Manipulation/BitwiseOp.java
@@ -0,0 +1,29 @@
+package Bit_Manipulation;
+
+public class BitwiseOp {
+    public static void main(String[] args) {
+        //Bitwise AND
+        System.out.println("Bitwise AND of 5 & 6:");
+        System.out.println(5 & 6);
+
+        //Bitwise OR
+        System.out.println("Bitwise OR of 3 |1:");
+        System.out.println(3 | 1);
+
+        //Bitwise XOR
+        System.out.println("Bitwise XOR of 5 ^ 6:");
+        System.out.println(5 ^ 6);
+
+        //Bitwise Left Shift
+        System.out.println(" Bitwise Left shift of 7<<2:");
+        System.out.println(7 << 2);
+
+        //Bitwise Right Shift
+        System.out.println("Bitwise Right Shift of 4>>3:");
+        System.out.println(4 >> 3);
+
+        //Binary one's complement or Binary Not
+        System.out.println("Binary Not of 15:");
+        System.out.println(~15);
+    }
+}
