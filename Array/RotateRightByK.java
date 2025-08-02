9b62f87b0c9b5f916eae27c256830dcb30d6ecd3
nikita-salunke4578 <nikitasalunke960@gmail.com>
2025-08-02 13:15:02 +0530

    Added Program to Rotate array to Rigth by k elements

---
+++ Array/RotateRightByK.java
@@ -0,0 +1,28 @@
+//Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+package Array;
+
+import java.sql.Array;
+import java.util.Arrays;
+
+public class RotateRightByK {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 2;
+        RotateRightByK_fun(arr, k);
+    }
+
+    private static void RotateRightByK_fun(int[] arr, int k) {
+        int n = arr.length;
+
+        for (int i = 0; i < k; i++) {
+            int temp = arr[n - 1];
+
+            for (int j = n - 1; j >= 1; j--) {
+                arr[j] = arr[j - 1];
+            }
+            arr[0] = temp;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
