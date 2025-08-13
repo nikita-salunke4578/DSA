25359f86c86b9459f663dd42f5e85d127f6f4d72
nikita-salunke4578 <nikitasalunke960@gmail.com>
2025-08-13 22:56:41 +0530

    Added Program for printing pattern

---
+++ Pattern/p_11_striver.java
@@ -0,0 +1,25 @@
+package Pattern;
+
+public class p_11_striver {
+    public static void main(String[] args) {
+        int rows = 5, column = 5;
+        printPattern(rows, column);
+    }
+
+    private static void printPattern(int rows, int column) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < column; j++) {
+                if (j - i <= 0) {
+                    if ((j + i) % 2 == 0) {
+                        System.out.print("1");
+                    } else {
+                        System.out.print("0");
+                    }
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
